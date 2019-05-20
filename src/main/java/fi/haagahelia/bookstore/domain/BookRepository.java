3a04a25205aa4a1dba53d62952a88e4dd13f9ac8
Olli Eloranta <olli@ollieloranta.fi>
2019-05-20 04:19:52 +0300

    Exercises week 4 (part 1) ready

--- src/main/java/fi/haagahelia/bookstore/domain/BookRepository.java
+++ src/main/java/fi/haagahelia/bookstore/domain/BookRepository.java
@@ -1,6 +1,7 @@
 package fi.haagahelia.bookstore.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package fi.haagahelia.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -73,4 +76,15 @@ public class BookController {
 		repository.save(book);
 		return "redirect:/booklist";
 	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>)repository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+	}
+	
 }
