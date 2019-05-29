9d903f149808dee86c0bccfd7bb6c7083b58dfb2
Olli Eloranta <olli@ollieloranta.fi>
2019-05-29 21:26:05 +0300

    Chapter 6 Securing the web application Part II ready

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 import fi.haagahelia.bookstore.domain.Category;
 import fi.haagahelia.bookstore.domain.CategoryRepository;
+import fi.haagahelia.bookstore.domain.User;
+import fi.haagahelia.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +20,7 @@ public class BookstoreApplication {
 		}
 	
 		@Bean
-		public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository) {
+		public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 			return(args) -> {
 				Category category1 = new Category("Novels");
 				Category category2 = new Category("Children's books");
@@ -29,6 +31,14 @@ public class BookstoreApplication {
 				Book book2 = new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 8.50, category1);
 				repository.save(book1);
 				repository.save(book2);
+				
+				User user1 = new User("user1", "$2a$10$WIGJD4RRdklqp7g7nrTsguPuZo4miWy.MvrOXpvBz6HBEBR.2.yc.", "user1@user.com", "USER");
+				User user2 = new User("user2", "$2a$10$H/sO5nJlaoQfVmm2Y7DN1uZzjGe5UK6F02IH5JrYp6WZJmNoZNR52", "user2@user.com", "USER");
+				User admin = new User("admin", "$2a$10$HQJWdbJzfwiQ9Wgn51SJIuerkNhAN4SqrqtLwybcMa.sVtHfje8JO", "admin@admin.com", "ADMIN");
+				urepository.save(user1);
+				urepository.save(user2);
+				urepository.save(admin);
+				
 			};
 		}
 
--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +39,7 @@ public class BookController {
 		return "booklist";
 	}
 	
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 		repository.deleteById(id);
